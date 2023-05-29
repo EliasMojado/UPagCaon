f4ba07979183218b97eb6e6d29ce2b131b130844
Daughpane Reponte <daughpanereponte@gmail.com>
2023-05-29 15:11:29 +0800

    modified landing

--- src/landing.js
+++ src/landing.js
@@ -1,17 +1,25 @@
-import logo from './logo.svg'
+import logo from './logo.svg';
+import kaha from './kaha.svg';
+import 'typeface-inter';
 
 function Landing(){
     return(
         <div className="landing">
             <header>
                 <div className="button-container">
-                    <button className="transparent-button">About us</button>
+                    <button className="transparent-button">ABOUT US</button>
                 </div>
             </header>
             <body>
                 <div className='img-container'>
                     <img src= {logo} alt="logo" className='logo' />
                 </div>
+                <div>                     
+                    <img src= {kaha} alt="kaha" className='kaha' />
+                </div>
+                <div className="login-button">
+                    <button className="login">LOG IN</button>
+                </div> 
             </body>
         </div>
     )
