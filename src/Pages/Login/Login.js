677667142a068cd7165ca22f25aa6031f292e305
efypuyot <92715199+efypuyot@users.noreply.github.com>
2023-05-29 15:56:47 +0800

    added Folders

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,9 @@
+function Login(){
+    return(
+        <div className="login-button">
+            LOOOOOOOOOOOOOOOOOOOOOOOOOOOOG IN NI
+        </div>
+    )
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
-import App from './App';
+import toast, { Toaster } from "react-hot-toast";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import "./landing.css";
+import './index.css';
+
 import Landing from './landing';
+import Login from './Pages/Login/Login';
+import "./landing.css";
+import "./Pages/Login/Login.css";
 
 import reportWebVitals from './reportWebVitals';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Landing />
+     <Router>
+      <Routes>
+        <Route path='/' element = {<Landing/>} />
+        <Route path='/login' element = {<Login/>} />
+      </Routes>
+     </Router>
   </React.StrictMode>
 );
 
