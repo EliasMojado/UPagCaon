8b23a3a4665b4be1664c6566eab8e4f31da2cd4b
Daughpane Reponte <daughpanereponte@gmail.com>
2023-05-30 20:44:39 +0800

    enabled CORS middleware

--- server/index.js
+++ server/index.js
@@ -6,6 +6,13 @@ const db = require('./db');
 
 app.use(express.json()); // Parse JSON request bodies
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}); // Enable CORS middleware
+
 // Define your routes
 app.use('/user', userRoutes);
 
