1bca9874e3f2c662cb55cb7c7e66814c99b35914
Daughpane Reponte <daughpanereponte@gmail.com>
2023-05-30 18:21:23 +0800

    close modal

--- src/Pages/Customer/Landing/LoginForm.js
+++ src/Pages/Customer/Landing/LoginForm.js
@@ -1,36 +1,45 @@
 import '../Landing/LoginForm.css';
-import React, {useState} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TextFieldComponent from '../Landing/LoginField.js';
 
 const LoginForm = ({ show, close }) => {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        close();
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [close]);
+
   return (
     <>
-     {
-     show ?
-
-      <div
-        className="modalContainer"
-        onClick={() => close()}
-      >
-        <div className="modal" onClick={(e) => e.stopPropagation()}>
-          <header className="modal_header">
-            <h2 className="modal_header-title">LOG IN</h2>
-          </header>
-          <main className="modal_content">
-          <div>
-            <TextFieldComponent />
+      {show ? (
+        <div className="modalContainer">
+          <div ref={modalRef} className="modal" onClick={(e) => e.stopPropagation()}>
+            <header className="modal_header">
+              <h2 className="modal_header-title">LOG IN</h2>
+            </header>
+            <main className="modal_content">
+              <div>
+                <TextFieldComponent />
+              </div>
+            </main>
+            <footer className="modal_footer">
+              <button className="login" onClick={close}>
+                Login
+              </button>
+            </footer>
+          </div>
         </div>
-          </main>
-          <footer className="modal_footer">
-            {/* <button className="signup">Sign up</button> */}
-            <button className="login" onClick={() => close()}>
-              Login
-            </button>
-          </footer>
-        </div>
-      </div>
-      : null
-     }
+      ) : null}
     </>
   );
 };
