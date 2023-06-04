3ae8f9a9e6cd8c11d44075ace6573844cab10c0a
John Elias Mojado <johnelijahmojado@gmail.com>
2023-06-04 14:20:17 +0800

    log out lang sah

--- src/Pages/Admin/Dashboard/Sidebar.js
+++ src/Pages/Admin/Dashboard/Sidebar.js
@@ -23,63 +23,82 @@ function Sidebar() {
       <div></div>
       <SideNav.Toggle />
       <SideNav.Nav defaultSelected="home">
-        <NavItem eventKey="home">
+    
+        <NavItem eventKey="home" onClick={home}>
           <NavIcon>
             <img src={home} alt="home" className="nav-icon" />
           </NavIcon>
           <NavText className="navtext">Home</NavText>
         </NavItem>
-        <NavItem eventKey="user">
+
+        <NavItem eventKey="user" onClick = {userBar}>
           <NavIcon>
             <img src={user} alt="user" className="nav-icon" />
           </NavIcon>
           <NavText className="navtext">User</NavText>
         </NavItem>
+
         <NavItem eventKey="viand">
           <NavIcon>
             <img src={viand} alt="viand" className="nav-icon" />
           </NavIcon>
           <NavText className="navtext">Viands</NavText>
         </NavItem>
+
         <NavItem eventKey="drink">
           <NavIcon>
             <img src={drink} alt="drink" className="nav-icon" />
           </NavIcon>
           <NavText className="navtext">Drinks</NavText>
         </NavItem>        
+
         <NavItem eventKey="snack">
           <NavIcon>
             <img src={snack} alt="snack" className="nav-icon" />
           </NavIcon>
           <NavText className="navtext">Snacks</NavText>
         </NavItem>
+
         <NavItem eventKey="schoolsupply">
           <NavIcon>
             <img src={schoolsupply} alt="schoolsupply" className="nav-icon" />
           </NavIcon>
           <NavText className="navtext">School Supplies</NavText>
         </NavItem>
+
         <NavItem eventKey="cart">
           <NavIcon>
             <img src={cart} alt="cart" className="nav-icon" />
           </NavIcon>
           <NavText className="navtext">Cart</NavText>
         </NavItem>
+
         <NavItem eventKey="wallet">
           <NavIcon>
             <img src={wallet} alt="wallet" className="nav-icon" />
           </NavIcon>
           <NavText className="navtext">Profit</NavText>
         </NavItem>
-        <NavItem eventKey="shutdown" className="shutdown">
+
+        <NavItem eventKey="shutdown" className="shutdown" onClick = {logout}>
           <NavIcon>
             <img src={shutdown} alt="shutdown" className="nav-icon" />
           </NavIcon>
           <NavText className="navtext">Log Out</NavText>
         </NavItem>
+
       </SideNav.Nav>
     </SideNav>
   );
 }
+
+const userBar = () =>{
+  console.log('napislit');
+}
+
+const logout = () =>{
+  window.location.href = '/admin';
+}
+
 
 export default Sidebar;
