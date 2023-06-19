b2abfd365b1daaabff77c82c5b51354a0ba84f9c
John Elias Mojado <johnelijahmojado@gmail.com>
2023-06-19 12:49:54 +0800

    image upload finally working

--- server/firebase.js
+++ server/firebase.js
@@ -1,25 +1,36 @@
-const firebase = require('firebase');
+// const firebase = require("firebase/app");
+// require("firebase/storage");
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDtrp7X52ywThTQncLWWZABJ1w3yptFz5c",
-    authDomain: "upagcaon.firebaseapp.com",
-    projectId: "upagcaon",
-    storageBucket: "upagcaon.appspot.com",
-    messagingSenderId: "630778761341",
-    appId: "1:630778761341:web:82e67cf884a83a7f9aa114",
-    measurementId: "G-SMHV78Z1YM"
-};
+// // Set the configuration for your app
+// // TODO: Replace with your app's config object
+// const firebaseConfig = {
+//   apiKey: "YOUR_API_KEY",
+//   authDomain: "upagcaon.firebaseapp.com",
+//   projectId: "upagcaon",
+//   storageBucket: "upagcaon.appspot.com",
+//   messagingSenderId: "630778761341",
+//   appId: "1:630778761341:web:82e67cf884a83a7f9aa114",
+//   measurementId: "G-SMHV78Z1YM"
+// };
 
-firebase.initializeApp(firebaseConfig);
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-firebase
-  .auth()
-  .signInAnonymously()
-  .then(() => {
-    console.log("Connected to Firebase successfully!");
-  })
-  .catch((error) => {
-    console.error("Error connecting to Firebase:", error);
-  })
+// // Get a reference to the storage service, which is used to create references in your storage bucket
+// const storage = firebase.storage();
 
-module.exports = firebase;
+// module.exports = storage;
+
+
+const admin = require("firebase-admin");
+
+// Set the configuration for your app
+const serviceAccount = require("../upagcaon-firebase-adminsdk-w3dp5-1554c78714.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "upagcaon.appspot.com" // Replace with your storage bucket URL
+});
+
+const storage = admin.storage();
+
+module.exports = storage;
--- server/item.js
+++ server/item.js
@@ -1,32 +1,127 @@
-const express = require('express');
-const multer = require('multer');
-const bodyParser = require('body-parser');
-const app = express();
-const db = require('./db');
-const uuid = require('uuid');
-const router = express.Router();
+// const express = require('express');
+// const multer = require('multer');
+// const bodyParser = require('body-parser');
+// const app = express();
+// const uuid = require('uuid');
+// const router = express.Router();
+// const upload = multer();
+// app.use(bodyParser.urlencoded({ extended: true }));
 
+// const db = require('./db');
+// const firebase = require('./firebase');
+
+// // POST /item - Add a new item
+// router.post('/add', upload.single('image'), (req, res) => {
+//   const { name, price, type, description, quantity, expiryDate } = req.body;
+//   const id = uuid.v4();
+
+//   const query = 'INSERT INTO items (id, name, price, type, description, quantity, expiry_date, image_url) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+//   const values = [id, name, price, type, description, quantity, expiryDate];
+//   const imageFile = req.file;
+//   if (!imageFile) {
+//     // Handle the case where no image file was uploaded
+//     console.log('No image file provided.');
+//     res.status(400).json({ error: 'No image file provided.' });
+//     return;
+//   }
+
+//   // Upload the image to Firebase Storage
+//   const storageRef = firebase.storage().ref();
+//   const imageName = `${id}-${imageFile.originalname}`;
+//   const imageRef = storageRef.child(imageName);
+//   const imageMetadata = { contentType: imageFile.mimetype };
+
+//   imageRef
+//     .put(imageFile.buffer, imageMetadata)
+//     .then((snapshot) => {
+//       // Get the download URL of the uploaded image
+//       return imageRef.getDownloadURL();
+//     })
+//     .then((imageUrl) => {
+//       // Store the image URL in the item table along with other attributes
+//       values.push(imageUrl);
+
+//       db.query(query, values, (err, results) => {
+//         if (err) {
+//           console.error('Error inserting item:', err);
+//           res.status(500).json({ error: 'An error occurred while inserting the item.' });
+//           return;
+//         }
+
+//         console.log('Item inserted successfully!');
+//         res.status(200).json({ message: 'Item inserted successfully!' });
+//       });
+//     })
+//     .catch((error) => {
+//       console.error('Error uploading image:', error);
+//       res.status(500).json({ error: 'An error occurred while uploading the image.' });
+//     });
+// });
+
+// module.exports = router;
+
+
+const express = require("express");
+const multer = require("multer");
+const bodyParser = require("body-parser");
+const app = express();
+const uuid = require("uuid");
+const router = express.Router();
 const upload = multer();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./db");
+const storage = require("./firebase");
+
 // POST /item - Add a new item
-router.post('/add', upload.single('image'), (req, res) => {
+router.post("/add", upload.single("image"), (req, res) => {
   const { name, price, type, description, quantity, expiryDate } = req.body;
   const id = uuid.v4();
 
-  const query = 'INSERT INTO items (id, name, price, type, description, quantity, expiry_date) VALUES (?, ?, ?, ?, ?, ?, ?)';
+  const query =
+    "INSERT INTO items (id, name, price, type, description, quantity, expiry_date, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
   const values = [id, name, price, type, description, quantity, expiryDate];
+  const imageFile = req.file;
+  if (!imageFile) {
+    // Handle the case where no image file was uploaded
+    console.log("No image file provided.");
+    res.status(400).json({ error: "No image file provided." });
+    return;
+  }
 
-  db.query(query, values, (err, results) => {
-    if (err) {
-      console.error('Error inserting item:', err);
-      res.status(500).json({ error: 'An error occurred while inserting the item.' });
-      return;
-    }
+  // Upload the image to Firebase Storage
+  const bucket = storage.bucket();
+  const imageName = `${id}-${imageFile.originalname}`;
+  const imageFileRef = bucket.file(imageName);
+  const imageMetadata = { contentType: imageFile.mimetype };
 
-    console.log('Item inserted successfully!');
-    res.status(200).json({ message: 'Item inserted successfully!' });
-  });
+  imageFileRef
+    .createWriteStream({
+      metadata: imageMetadata
+    })
+    .on("error", (error) => {
+      console.error("Error uploading image:", error);
+      res.status(500).json({ error: "An error occurred while uploading the image." });
+    })
+    .on("finish", () => {
+      // Get the download URL of the uploaded image
+      const imageUrl = `https://storage.googleapis.com/${bucket.name}/${imageName}`;
+
+      // Store the image URL in the item table along with other attributes
+      values.push(imageUrl);
+
+      db.query(query, values, (err, results) => {
+        if (err) {
+          console.error("Error inserting item:", err);
+          res.status(500).json({ error: "An error occurred while inserting the item." });
+          return;
+        }
+
+        console.log("Item inserted successfully!");
+        res.status(200).json({ message: "Item inserted successfully!" });
+      });
+    })
+    .end(imageFile.buffer);
 });
 
 module.exports = router;
