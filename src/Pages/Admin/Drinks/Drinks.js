0a7461b4a49a7422d2a6016914ac8e52f34d19fd
John Elias Mojado <johnelijahmojado@gmail.com>
2023-06-20 13:23:31 +0800

    drinks are now dynamic

--- src/Pages/Admin/Drinks/Drinks.js
+++ src/Pages/Admin/Drinks/Drinks.js
@@ -1,25 +1,30 @@
-import React, { useState }from 'react';
+import React, { useEffect, useState }from 'react';
 import Sidebar from "../Dashboard/Sidebar";
 import SearchBar from "../Dashboard/SearchBar";
 import '../Drinks/Drinks.css';
 import DrinksList from './DrinksList';
 import AddDrinkModal from './AddDrinkModal';
 import withAdminAuthentication from "../requireAdminAuthentication";
+import { getItem } from '../items';
 
 function Drinks() {
-    const drinks = [
-        { id: 1271672482, type: 'Drink', name: 'Coffee Jelly', description: 'mainom', price: 30, quantity: 20, expiry: '06-18-2023', image: 'img' },
-        { id: 2372837232, type: 'Drink', name: 'Mango Tapioca', description: 'mainom', price: 20, quantity: 20, expiry: '06-18-2023', image: 'img' },
-        { id: 3323436563, type: 'Drink', name: 'Coca Cola', description: 'mainom', price: 35, quantity: 20, expiry: '06-18-2023', image: 'img' },
-        { id: 4696856604, type: 'Drink', name: 'Cucumber Lemonade', description: 'mainom', price: 20, quantity: 20, expiry: '06-18-2023', image: 'img' },
-        { id: 5583983975, type: 'Drink', name: 'Sprite', description: 'mainom', price: 25, quantity: 20, expiry: '06-18-2023', image: 'img' },
-        { id: 6454954586, type: 'Drink', name: 'Pepsi', description: 'mainom', price: 30, quantity: 20, expiry: '06-18-2023', image: 'img' },
-        { id: 7584549557, type: 'Drink', name: 'C2', description: 'mainom', price: 35, quantity: 20, expiry: '06-18-2023', image: 'img' },
-      ];
+    const [showAddDrinkModal, setShowAddDrinkModal] = useState(false);
+    const [drinks, setDrinks] = useState([]);
+    const Toggle = () => setShowAddDrinkModal(!showAddDrinkModal);
+    const closeAddDrinkModal = () => setShowAddDrinkModal(false);
 
-      const [showAddDrinkModal, setShowAddDrinkModal] = useState(false);
-      const Toggle = () => setShowAddDrinkModal(!showAddDrinkModal);
-      const closeAddDrinkModal = () => setShowAddDrinkModal(false);
+    useEffect(() =>{
+    const fetchData = async () => {
+        try {
+            const data = await getItem('drink');
+            setDrinks(data);
+        }catch (error){
+            console.error('Error:', error);
+        }
+    };
+
+    fetchData();
+    }, []);
   
     return (
         <div className="drinks-page">
