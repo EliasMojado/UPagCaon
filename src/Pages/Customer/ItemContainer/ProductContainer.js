daf8048d6f10981080eda3ea9fdacb7bc6fe8ac1
efypuyot <92715199+efypuyot@users.noreply.github.com>
2023-06-25 14:38:40 +0800

    Product Page rows and cols

--- src/Pages/Customer/ItemContainer/ProductContainer.js
+++ src/Pages/Customer/ItemContainer/ProductContainer.js
@@ -2,11 +2,12 @@ import React from 'react';
 import '../Product/ProductPage.css';
 import rating from '../ItemContainer/Rating';
 
-const ProductContainer = ({ imageSrc, title, rating, price }) => {
+const ProductContainer = ({ imageSrc, name, rating, price }) => {
   return (
+    
     <div className='p-container'>
       <img src={imageSrc} alt="Container Image" className='product-img' />
-      <h2 className='title'>{title}</h2>
+      <h2 className='title'>{name}</h2>
       <div className='rate'>
           {rating.map((star, index) => (
             <img key={index} src={star} alt='star' className={`rating ${index === 0 ? 'first-star' : ''}`}/>
