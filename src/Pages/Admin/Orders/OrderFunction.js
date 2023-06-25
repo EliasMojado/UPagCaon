6ec69e7fe73efce4ea2bd159ee120b67ecd4bb82
John Elias Mojado <johnelijahmojado@gmail.com>
2023-06-25 15:40:16 +0800

    order status updates

--- src/Pages/Admin/Orders/OrderFunction.js
+++ src/Pages/Admin/Orders/OrderFunction.js
@@ -30,3 +30,27 @@ export function getOrderedItems(id) {
       // Handle the error case appropriately
     });
 }
+
+export function updateOrderStatus(id, status) {
+  const requestOptions = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ decision: status }),
+  };
+
+  return fetch(`/updateOrderStatus/${id}`, requestOptions)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to update order status');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Order status updated successfully:', data.message);
+      return data;
+    })
+    .catch(error => {
+      console.error('Error updating order status:', error);
+      throw error;
+    });
+}
