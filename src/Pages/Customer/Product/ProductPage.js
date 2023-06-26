3c5d676fb2a33f96d25f6f606724aa3a4c8dea5e
Daughpane Reponte <daughpanereponte@gmail.com>
2023-06-26 21:19:16 +0800

    working searchbar for products

--- src/Pages/Customer/Product/ProductPage.js
+++ src/Pages/Customer/Product/ProductPage.js
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import ProductContainer from '../ItemContainer/ProductContainer';
 import Sidebar from '../Home/Sidebar';
 import SearchBar from '../Home/SearchBar';
@@ -7,20 +7,31 @@ import Rating from '../ItemContainer/Rating';
 import './ProductPage.css';
 
 function ProductPage({ title, products }) {
+  const navigate = useNavigate();
+  const [filteredProducts, setFilteredProducts] = useState(products);
+
   useEffect(() => {
     // Retrieve the stored user data from local storage
     const storedUser = localStorage.getItem('user');
     if (!storedUser || JSON.parse(storedUser).type !== 'customer') {
-      window.location.href = '/';
+      navigate('/');
     }
-  }, []);
+  }, [navigate]);
+
+  useEffect(() => {
+    setFilteredProducts(products);
+  }, [products]);
+
+  const handleSearch = (filteredItems) => {
+    setFilteredProducts(filteredItems);
+  };
 
   const renderProducts = () => {
     const productRows = [];
     const columnsPerRow = 4;
 
-    for (let i = 0; i < products.length; i += columnsPerRow) {
-      const productRow = products.slice(i, i + columnsPerRow);
+    for (let i = 0; i < filteredProducts.length; i += columnsPerRow) {
+      const productRow = filteredProducts.slice(i, i + columnsPerRow);
       const productColumns = productRow.map((product, index) => (
         <div key={index} className="product-column">
           <ProductContainer
@@ -38,31 +49,33 @@ function ProductPage({ title, products }) {
         </div>
       );
     }
-
     return productRows;
   };
 
-    
-  const navigate = useNavigate();
-
-  const Toggle = () => {
-    navigate('/cart', { replace: true});
-  }
+  const handleCartClick = () => {
+    navigate('/cart', { replace: true });
+  };
 
   return (
     <div className="dashboard">
       <header className="header-container">
         <span className="dash">{title.toUpperCase()}</span>
-        <button className="add-order-cart" onClick={() => Toggle()}>
-            Cart
-      </button>
-        <SearchBar />
-        <Sidebar />
+        <button className="add-order-cart" onClick={handleCartClick}>
+          Cart
+        </button>
+        <SearchBar
+          items={products}
+          setFilteredItems={handleSearch}
+          itemType="items"
+        />
+        <Sidebar
+          items={products}
+          setFilteredItems={handleSearch}
+          itemType="items"
+        />
       </header>
       <div className="product-containers">
-      <div className='product-cont'>
-          {renderProducts()}
-      </div>
+        <div className="product-cont">{renderProducts()}</div>
       </div>
     </div>
   );
