fe1f240832cdc2e480bb0ef755e21387625d1bf9
John Elias Mojado <johnelijahmojado@gmail.com>
2023-06-27 12:14:17 +0800

    minor stuff'

--- server/firebase.js
+++ server/firebase.js
@@ -1,26 +1,3 @@
-// const firebase = require("firebase/app");
-// require("firebase/storage");
-
-// // Set the configuration for your app
-// // TODO: Replace with your app's config object
-// const firebaseConfig = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "upagcaon.firebaseapp.com",
-//   projectId: "upagcaon",
-//   storageBucket: "upagcaon.appspot.com",
-//   messagingSenderId: "630778761341",
-//   appId: "1:630778761341:web:82e67cf884a83a7f9aa114",
-//   measurementId: "G-SMHV78Z1YM"
-// };
-
-// const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-// // Get a reference to the storage service, which is used to create references in your storage bucket
-// const storage = firebase.storage();
-
-// module.exports = storage;
-
-
 const admin = require("firebase-admin");
 
 // Set the configuration for your app
